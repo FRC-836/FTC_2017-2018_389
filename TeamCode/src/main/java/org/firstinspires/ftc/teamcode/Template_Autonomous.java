90796b315bb7ee16260a82828bdf3e8e0580754c
Cody Graham <codydgraham@gmail.com>
2018-02-20 18:42:12 -0500

    Reverted to basic class structure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Template_Autonomous.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Template Autonomous", group="Template")
+public class Template_Autonomous extends Autonomous_Parent {
+
+    @Override
+    public void runAutonomous() {
+
+    }
+}
