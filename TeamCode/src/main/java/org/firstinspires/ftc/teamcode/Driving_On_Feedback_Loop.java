1ecdaef131de11dbf0b37aff234d8b52d7a83d04
FTC389 (proxy Raymond Tuazon) <rrttuazon@gmail.com>
2018-03-08 20:02:01 -0500

    Began PID control for lift.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driving_On_Feedback_Loop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driving_On_Feedback_Loop.java
@@ -1,14 +1,62 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Driving_On_Feedback_Loop", group="Test")
 public class Driving_On_Feedback_Loop extends Autonomous_Parent {
 
+    ElapsedTime runtime;
+
+    int input = 0;
+    double output = 0.0;
+    int setpoint = 0;
+    int error = 0;
+    double time = 0.0;
+    int lastError = 0;
+    double lastTime = 0.0;
+    double pValue = 0.0;
+    double iValue = 0.0;
+    double dValue = 0.0;
+    double pGain = 0.0;
+    double iGain = 0.0;
+    double dGain = 0.0;
+
+    double ENCODER_COUNTS_PER_DEGREE = 0.0; // TODO: Find this value (Lift)
+
     @Override
     public void runAutonomous() {
-        
+        liftArmPID(90.0);
+    }
 
+    public void liftArmPID(double angles_degrees) {
+        resetArmEncoder();
+        boolean pidIsRunning = true;
+        setpoint = (int) Math.round(angles_degrees * ENCODER_COUNTS_PER_DEGREE);
+        while (pidIsRunning) {
+            input = liftMotor.getCurrentPosition();
+            error = setpoint - input;
+            pValue = pGain * error;
 
+            // TODO: Find iValue and dValue
+
+            output = pValue + iValue + dValue;
+            setDrive(output, output);
+        }
+    }
+
+    public void resetDriveEncoders(){
+        backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        sleep(250); // TODO: Remove this sleep if possible
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void resetArmEncoder(){
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        sleep(250); // TODO: Remove this sleep if possible
+        liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 }
